f7e58a5749d9d96668f62e8f88322ee83ba450bd
SRILEKHASRILEKHA <144771696+SRILEKHASRILEKHA@users.noreply.github.com>
2023-09-21 11:13:30 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,20 +1,36 @@
- # Leap year
+class BankAccount:
+    def _init_(self, account_number, account_holder_name, initial_balance=0):
+        self.__account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
 
-"""
- year % 4 == 0 &
- year % 100 !=0 /
- year % 400 == 0
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            print(f"Deposited ${amount}. New balance: ${self.__account_balance}")
+        else:
+            print("Invalid deposit amount. Please enter a positive amount.")
 
- """
-def isLeapyear(year):
-  if (year % 4 == 0 and year % 100 !=0) or year % 400 == 0:
-    return True
-  else:
-    return False 
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self.__account_balance:
+            self.__account_balance -= amount
+            print(f"Withdrew ${amount}. New balance: ${self.__account_balance}")
+        else:
+            print("Invalid withdrawal amount or insufficient balance.")
 
-year = 2012
+    def display_balance(self):
+        print(f"Account Holder: {self.__account_holder_name}")
+        print(f"Account Number: {self.__account_number}")
+        print(f"Account Balance: ${self.__account_balance}")
 
-if isLeapyear(year):
- print('{} is a leap year .'.format(year))
-else:
-  print('{} is not a leap year .'.format(year))
+
+# Example usage:
+if __name__ == "__main__":
+    # Create a BankAccount instance
+    account = BankAccount("123456789", "John Doe", 1000)
+
+    # Test deposit and withdrawal
+    account.display_balance()
+    account.deposit(500)
+    account.withdraw(200)
+    account.display_balance()
